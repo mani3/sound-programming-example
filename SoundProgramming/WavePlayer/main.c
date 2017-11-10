207e81ea1334e43884079319f285d490dea68006
Kazuya Shida <kazuya.4da@gmail.com>
2017-11-10 18:58:23 +0900

    Add portaudio path

---
+++ SoundProgramming/WavePlayer/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  WavePlayer
+//
+//  Created by Kazuya Shida on 2017/11/09.
+//  Copyright © 2017 mani3. All rights reserved.
+//
+
+#include <stdio.h>
+#include "portaudio.h"
+
+int main(int argc, const char * argv[]) {
+    
+    PaStreamParameters outputParamaters;
+    PaStream *stream;
+    PaError error;
+    
+    printf("Hello, World!\n");
+    return 0;
+}
